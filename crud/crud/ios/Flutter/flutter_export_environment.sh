7dacce288d93a0610a7d50831451c408f636d5d7
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-05-29 18:23:37 +0530

    Add files via upload

---
+++ crud/crud/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/diluka/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/diluka/Documents/Documents - Diluka’s MacBook Air - 1/My Documents/My Files/NSBM/Lectures/Mobile Application Development/Session 02/crud"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
